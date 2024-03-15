75cc9168cdeb6c48b546bdaf97e513a9bd62c230
BLOODY2410 <72750703+BLOODY2410@users.noreply.github.com>
2024-03-15 12:44:46 +0200

    Add files via upload

---
+++ ProjectKA-var1.1/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> lines = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
